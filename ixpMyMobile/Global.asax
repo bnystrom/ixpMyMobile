1df3b2dac4a9a391f171f987fda83093e4400dbc
Bryan Nystrom <bnystrom@gmail.com>
2017-08-04 11:54:02 -0500

    Add project files.

---
+++ ixpMyMobile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ixpMyMobile.MvcApplication" Language="C#" %>
---
+++ ixpMyMobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ixpMyMobile.Startup))]
+namespace ixpMyMobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
